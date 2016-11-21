f9ba808bb70ab201b0baac0deaa2895ee5c95bdf
melisserrr <mtsnider@gmail.com>
2016-11-21 11:56:14 -0600

    added login fuctionality

--- app/src/main/java/com/strangerandglory/one/LoginActivity.java
+++ app/src/main/java/com/strangerandglory/one/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.strangerandglory.one;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String TAG = "Login";
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +41,12 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (mAuth.getCurrentUser() != null) {
-
                     Log.d("Login", "onAuthStateChanged:signed_in:");
-
-
                 } else {
                     // User is signed out
                     Log.d("Login", "onAuthStateChanged:signed_out");
                 }
 
-                //updateUI(user);
 
             }
         };
@@ -55,6 +55,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                signIn(email.getText().toString(), password.getText().toString());
+
             }
         });
 
@@ -64,6 +65,26 @@ public class LoginActivity extends AppCompatActivity {
                 register(email.getText().toString(), password.getText().toString());
             }
         });
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.sendPasswordResetEmail(email.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d("LOG", "We have sent you instructions to reset your password!");
+                                } else {
+                                    Log.d("LOG", "Failed to send reset email!");
+                                }
+
+                            }
+                        });
+            }
+        });
+
+
     }
 
 
@@ -73,12 +94,19 @@ public class LoginActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        sharedPreferences = getSharedPreferences("main", MODE_PRIVATE);
                         Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
                         Log.d(TAG, mAuth.getCurrentUser().getEmail());
                         Log.d(TAG, mAuth.getCurrentUser().getUid());
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
+                        sharedPreferences = getSharedPreferences("main", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("name",mAuth.getCurrentUser().getDisplayName() );
+                        editor.putString("email",mAuth.getCurrentUser().getEmail() );
+                        editor.putString("uid",mAuth.getCurrentUser().getUid() );
+                        editor.commit();
+
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
 
 
                         if (!task.isSuccessful()) {
@@ -96,11 +124,6 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-
                     }
                 });
     }
